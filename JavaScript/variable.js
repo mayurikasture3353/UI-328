614e6fe5f688add837ec7e6c010b7fcceeb574ae
mayurikasture3353 <mayurikasture3001@gmail.com>
2025-07-19 13:06:15 +0530

    create js files

---
+++ JavaScript/variable.js
@@ -0,0 +1,34 @@
+console.log(20+30);
+
+
+/**---------------- Data Type - Number------------**/
+var a= 180;
+var b = 30;
+Ans = a*b;  
+Another = a/b;
+document.write(Ans + "<br>");
+document.write(Another + "<br>");
+
+/**---------------- Data Type - String------------**/
+
+var m = "Welcome to Developer Guru Family..!"
+document.write(m + "<br>");
+
+/**---------------- Data Type - String - Concatenation------------**/
+
+var name1 = "Mayuri";
+var name2 = "Kasture";
+Name = name1 +  " " + name2;
+document.write(Name + "<br>");
+
+/**---------------- Data Type - Array------------**/
+
+var k = [10, 20, 30, 40, 50, 60, "Mayuri"];
+document.write(k[6] + "<br>");
+console.log(k);
+
+/**---------------- Data Type - Object------------**/
+
+var s = {H: "HTML", C: "CSS", B: "Bootstrap", JS: "JavaScript"}
+document.write(s.JS);
+console.log(s);
